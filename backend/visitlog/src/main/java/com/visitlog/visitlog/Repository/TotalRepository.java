80ef3936334ce74838d68f7d85fe522e0db0c73c
Kim Dong Geun <37145713+ehdrms2034@users.noreply.github.com>
2020-09-28 18:44:50 +0900

    Revert "[Feature] Config Server 보일러 플레이트"

--- backend/visitlog/src/main/java/com/visitlog/visitlog/Repository/TotalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.visitlog.visitlog.Repository;
-
-import com.visitlog.visitlog.Model.Total;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TotalRepository extends CrudRepository<Total,Long> {
-
-}
