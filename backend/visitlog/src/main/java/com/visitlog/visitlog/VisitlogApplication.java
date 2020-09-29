95f057a027b8b1cfedfe198e7dfbead450022a4c
KimDongGeun <ehdrms2034@naver.com>
2020-09-29 15:28:35 +0900

    [Feature] ConfigServer  info

--- backend/visitlog/src/main/java/com/visitlog/visitlog/VisitlogApplication.java
+++ backend/visitlog/src/main/java/com/visitlog/visitlog/VisitlogApplication.java
@@ -2,8 +2,10 @@ package com.visitlog.visitlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class VisitlogApplication {
 
     public static void main(String[] args) {
